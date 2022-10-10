304701582dc6b27a37db0c2b96c6ec8168ff218f
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2022-10-10 22:46:42 +0200

    build: use multiarch manifest

--- flake.nix
+++ flake.nix
@@ -59,17 +59,17 @@
           src = ./.;
         };
 
-        packages.docker-image-amd64 = pkgs.dockerTools.buildImage {
-          name = "svarozhits";
-          tag = "latest";
+        packages.docker-image-amd64 = pkgs.dockerTools.buildLayeredImage {
+          name = "ghcr.io/raroh73/svarozhits";
+          tag = "amd64";
           config = {
             Cmd = [ "${packages.x86_64-unknown-linux-gnu}/bin/svarozhits" ];
           };
         };
 
-        packages.docker-image-arm64 = pkgs.pkgsCross.aarch64-multiplatform.dockerTools.buildImage {
-          name = "svarozhits";
-          tag = "latest";
+        packages.docker-image-arm64 = pkgs.pkgsCross.aarch64-multiplatform.dockerTools.buildLayeredImage {
+          name = "ghcr.io/raroh73/svarozhits";
+          tag = "arm64";
           config = {
             Cmd = [ "${packages.aarch64-unknown-linux-gnu}/bin/svarozhits" ];
           };
