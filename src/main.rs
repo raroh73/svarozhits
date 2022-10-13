b3d22a870bea51f70ced3770562d9c633d20972a
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2022-10-13 16:54:16 +0200

    feat: add basic axum code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Server};
+use std::{error::Error, net::SocketAddr};
+use tracing::info;
+
+async fn root() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    info!("Listening on {}", addr);
+
+    Server::bind(&addr).serve(app.into_make_service()).await?;
+
+    Ok(())
 }
