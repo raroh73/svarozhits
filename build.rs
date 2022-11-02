1c9c8b382e91d129451d99a9661854b03b596492
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2022-11-02 15:39:15 +0100

    build: rewrite build.rs

--- build.rs
+++ build.rs
@@ -1,17 +1,19 @@
 use minify_html::{minify, Cfg};
-use std::fs;
+use std::{error::Error, fs};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cfg = Cfg::new();
 
     fs::write(
         "templates/base.html",
-        minify(&fs::read("templates/src/base.html").unwrap(), &cfg),
-    )
-    .unwrap();
+        minify(&fs::read("templates/src/base.html")?, &cfg),
+    )?;
     fs::write(
         "templates/index.html",
-        minify(&fs::read("templates/src/index.html").unwrap(), &cfg),
-    )
-    .unwrap();
+        minify(&fs::read("templates/src/index.html")?, &cfg),
+    )?;
+
+    println!("cargo:rerun-if-changed=templates/src");
+
+    Ok(())
 }
